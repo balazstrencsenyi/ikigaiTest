887ac379f2b609d4eb8fb2cd4aadb6e7676bfda2
balazstrencsenyi <b.trencsenyi@gmail.com>
2024-02-25 23:06:07 +0100

    base

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -5,7 +5,12 @@ function App() {
   return (
     <div className="App">
       <div className="Header">
-      
+        <div className="menu">Home</div>
+        <div className="menu">Services</div>
+        <div className="menu">Benefits</div>
+        <div className="menu">Team</div>
+        <div className="menu">Blog</div>
+        <div className="menu">Contact</div>
       </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
