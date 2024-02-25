1124329d74e9b0ca67566502f190a5d862f9b9f7
balazstrencsenyi <b.trencsenyi@gmail.com>
2024-02-25 22:36:14 +0100

    background check

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,23 +1,16 @@
 import logo from "./logo.png";
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Under construction.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+
+      <div className="Page2"></div>
+
+      <div className="Page3"></div>
     </div>
   );
 }
