cdb5da575cece32738741879410799e5fb46b47b
balazstrencsenyi <b.trencsenyi@gmail.com>
2024-02-25 23:16:11 +0100

    adds scrolling animation

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,7 +1,23 @@
+import React, { useEffect } from "react";
 import logo from "./logo.png";
 import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const header = document.querySelector(".Header");
+
+    const scrollHandler = () => {
+      const scrollPos = window.scrollY;
+      const opacity = Math.min(scrollPos / 400, 1); // Change 200 to adjust when the transition completes
+      header.style.backgroundColor = `rgba(2, 126, 228, ${0.8 + opacity * 0.2})`; // Gradually increase opacity from 0.8 to 1
+    };
+
+    window.addEventListener("scroll", scrollHandler);
+
+    // Cleanup event listener on component unmount
+    return () => window.removeEventListener("scroll", scrollHandler);
+  }, []);
+
   return (
     <div className="App">
       <div className="Header">
