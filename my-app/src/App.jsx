8e62d84c14441a7db4489396f579c7aedd898011
balazstrencsenyi <b.trencsenyi@gmail.com>
2024-02-25 23:47:53 +0100

    separated Header.jsx

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,33 +1,12 @@
-import React, { useEffect } from "react";
 import logo from "./logo.png";
+import Header from "./components/Header"
 import "./App.css";
 
 function App() {
-  useEffect(() => {
-    const header = document.querySelector(".Header");
-
-    const scrollHandler = () => {
-      const scrollPos = window.scrollY;
-      const opacity = Math.min(scrollPos / 400, 1); // Adjust speed here
-      header.style.backgroundColor = `rgba(2, 126, 228, ${0.5 + opacity * 0.5})`; // Gradually increase opacity from 0.2 to 1
-    };
-
-    window.addEventListener("scroll", scrollHandler);
-
-    // Cleanup event listener on component unmount
-    return () => window.removeEventListener("scroll", scrollHandler);
-  }, []);
 
   return (
     <div className="App">
-      <div className="Header">
-        <div className="menu">Home</div>
-        <div className="menu">Services</div>
-        <div className="menu">Benefits</div>
-        <div className="menu">Team</div>
-        <div className="menu">Blog</div>
-        <div className="menu">Contact</div>
-      </div>
+      <Header />
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header>
